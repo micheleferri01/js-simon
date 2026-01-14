97faa36abe8091ad4d1d2438c9128c189e75cb35
micheleferri01 <micheleferri095@gmail.com>
2026-01-14 20:03:13 +0100

    impostati controlli per i dati

--- js/script.js
+++ js/script.js
@@ -21,12 +21,7 @@ countdown.innerText = countdownCounter;
 
 //* generare 5 numeri random
 const randomNumbers = [];
-
-for(let i = 0; i < 5; i++){
-    const randomNum = Math.floor(Math.random() * (50 - 1 + 1) + 1);
-    randomNumbers.push(`${randomNum}`);
-}
-console.log(randomNumbers);
+generateRandomNum();
 
 //* aggiugere i numeri generati al DOM
 for(let i = 0; i < randomNumbers.length; i++){
@@ -51,18 +46,68 @@ function timer(){
         clearInterval(intervalId);
     }
 }
+
+function generateRandomNum(){
+    while (randomNumbers.length < 5) {
+        const randomNum = Math.floor(Math.random() * (50 - 1 + 1) + 1);
+        let alreadyExists = false;
+        
+        for (let i = 0; i < randomNumbers.length; i++) {
+            if (randomNumbers[i] === randomNum) {
+                alreadyExists = true;
+            }
+        }
+
+        if (!alreadyExists){
+            randomNumbers.push(randomNum);
+        }
+    }
+
+    console.log(randomNumbers);
+}
+
 //* visualizzare il form da compilare per l'utente
-
-// array per salvare le risposte dell'utente
-
 function answers(){
     //* leggere il form al submit
     answersForm.addEventListener("submit", (e) =>{
         e.preventDefault();
+        answersHandler();
     })
     answersForm.classList.remove("d-none");
     
 }
 
-//* controllare i valori inseriti dall'utente
-//* visualizzare un messaggio con quanti numeri ha indovinato l'utente
+
+// array per salvare le risposte dell'utente
+function answersHandler(){
+    // contatore per risposte esatte
+    let correctAnswersCounter = 0;
+    const inputs = [];
+    
+    for (let i = 0; i < inputGroup.children.length ;i++){
+        const currentInput = parseInt(inputGroup.children[i].value);
+        //* controllare i valori inseriti dall'utente
+        // controllo valore valido
+        if (isNaN(currentInput)) {
+            message.innerText = "Inserisci solo numeri validi!";
+            return;
+        }
+
+        // controllo duplicati
+        for (let j = 0; j < inputs.length; j++) {
+            if (inputs[j] === currentInput) {
+                message.innerText = "Non puoi inserire lo stesso numero due volte!";
+                return;
+            }
+        }
+
+        inputs.push(currentInput);
+        for (let i = 0; i < randomNumbers.length; i++) {
+            if (currentInput === randomNumbers[i]) {
+                correctAnswersCounter++;
+            }
+        }
+        //* visualizzare un messaggio con quanti numeri ha indovinato l'utente
+        message.innerText = `Hai indovinato ${correctAnswersCounter} numeri.`;
+    }
+}
