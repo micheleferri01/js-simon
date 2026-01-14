9f635548c709950b70268f133ddda1288d03eb90
micheleferri01 <micheleferri095@gmail.com>
2026-01-14 14:30:26 +0100

    individuazione elementi DOM

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Elementi DOM
+const countdown = document.getElementById("countdown");
+const instructions = document.getElementById("instructions");
+const numberslist = document.getElementById("numbers-list");
+const answersForm = document.getElementById("answers-form");
+const inputGroup = document.getElementById("input-group");
+
