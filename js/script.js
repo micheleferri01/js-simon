74512e02c617e1c8b0d2627caf4e4230b4629a86
micheleferri01 <micheleferri095@gmail.com>
2026-01-14 15:35:52 +0100

    visualizzazione timer e generazione numeri

--- js/script.js
+++ js/script.js
@@ -5,13 +5,30 @@ const numberslist = document.getElementById("numbers-list");
 const answersForm = document.getElementById("answers-form");
 const inputGroup = document.getElementById("input-group");
 
+console.log(countdown);
+console.log(instructions);
+console.log(numberslist);
+console.log(answersForm);
+console.log(inputGroup);
+
 // todo:
-// visualizzare timer nel DOM
-// generare 5 numeri random
-// aggiugere i numeri generati al DOM
-// avviare timer ed impostare condizioni di fine countdown
-// nascondere i numeri generati in modo che l'utente non possa più vederli
-// visualizzare il form da compilare per l'utente
-// leggere il form al submit
-// controllare i valori inseriti dall'utente
-// visualizzare un messaggio con quanti numeri ha indovinato l'utente
+//* visualizzare timer nel DOM
+// contatore per countdown
+let countdownCounter = 30;
+
+countdown.innerText = countdownCounter;
+
+//* generare 5 numeri random
+const randomNumbers = [];
+for(let i = 0; i < 5; i++){
+    const randomNum = Math.floor(Math.random() * (50 - 1 + 1) - 1);
+    randomNumbers.push(`${randomNum}`);
+}
+console.log(randomNumbers);
+//* aggiugere i numeri generati al DOM
+//* avviare timer ed impostare condizioni di fine countdown
+//* nascondere i numeri generati in modo che l'utente non possa più vederli
+//* visualizzare il form da compilare per l'utente
+//* leggere il form al submit
+//* controllare i valori inseriti dall'utente
+//* visualizzare un messaggio con quanti numeri ha indovinato l'utente
