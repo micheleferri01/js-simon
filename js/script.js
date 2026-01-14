5fd3013f09c7d1cbbfcaf3b2e2c4123ee34a1b62
micheleferri01 <micheleferri095@gmail.com>
2026-01-14 15:15:48 +0100

    lista passaggi da fare

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ const numberslist = document.getElementById("numbers-list");
 const answersForm = document.getElementById("answers-form");
 const inputGroup = document.getElementById("input-group");
 
+// todo:
+// visualizzare timer nel DOM
+// generare 5 numeri random
+// aggiugere i numeri generati al DOM
+// avviare timer ed impostare condizioni di fine countdown
+// nascondere i numeri generati in modo che l'utente non possa più vederli
+// visualizzare il form da compilare per l'utente
+// leggere il form al submit
+// controllare i valori inseriti dall'utente
+// visualizzare un messaggio con quanti numeri ha indovinato l'utente
